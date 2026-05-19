ee2cb2b0f274f0f555f4594a8cb308648b766cbe
Kashyap Patel <kashyap.jsdev@gmail.com>
2026-05-19 11:32:34 +0530

    Repo initialized and first commit pushed

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,13 +3,14 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'trzso3lm',
-    dataset: 'production'
+    dataset: 'production',
   },
   deployment: {
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
+    appId: 'vqkzjky81j9yl6r60bqstcpz',
     autoUpdates: true,
-  }
+  },
 })
